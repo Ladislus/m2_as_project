0fedf68140c82e0c302b4d80353e0ad807efdc99
Ladislus <walcak.ladislas@gmail.com>
2021-11-19 00:23:01 +0100

    [FIX] Added missing optional identifier for program

--- src/main/java/ast/Program.kt
+++ src/main/java/ast/Program.kt
@@ -4,6 +4,7 @@ import ast.statement.Statement
 
 class Program(
         position: Position,
+        val _identifier: String?,
         val _procedures: List<Procedure>,
         val _variables: List<Variable>,
         val _statements: List<Statement>
