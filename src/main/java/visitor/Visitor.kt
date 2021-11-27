7255a2f950516cf5ca447c66e1aff22c4ee78a0f
Ladislus <walcak.ladislas@gmail.com>
2021-11-27 14:49:35 +0100

    [MOD] Updated Visitor interface to represent new AST classes

--- src/main/java/visitor/Visitor.kt
+++ src/main/java/visitor/Visitor.kt
@@ -11,15 +11,19 @@ interface Visitor<T> {
     fun visit(program: Program): T
     fun visit(procedure: Procedure): T
     fun visit(type: Type): T
+    fun visit(position: Position): T
     fun visit(variable: Variable): T
-    fun visit(declaration: Declaration): T
+    fun visit(variableBlock: VariableBlock): T
     fun visit(expression: Expression): T
     fun visit(arithmeticExpression: ArithmeticExpression): T
     fun visit(unaryArithmeticExpression: UnaryArithmeticExpression): T
     fun visit(binaryArithmeticExpression: BinaryArithmeticExpression): T
+    fun visit(arithmeticConstant: ArithmeticConstant): T
+    fun visit(arithmeticIdentifierExpression: IdentifierExpression): T
     fun visit(booleanExpression: BooleanExpression): T
     fun visit(unaryBooleanExpression: UnaryBooleanExpression): T
     fun visit(binaryBooleanExpression: BinaryBooleanExpression): T
+    fun visit(booleanConstant: BooleanConstant): T
     fun visit(statement: Statement): T
     fun visit(assignStatement: AssignStatement): T
     fun visit(callStatement: CallStatement)
