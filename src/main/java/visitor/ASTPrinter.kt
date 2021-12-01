c92b6e63fbbc556726a53d652df50a9fdfa53244
Ladislus <walcak.ladislas@gmail.com>
2021-12-01 06:30:46 +0100

    [ADD] ASTPrinter v0.1  - Untested  - Not working because of FIXME inside ASTBuilder (can't cast VariableBlock to Variable)

--- src/main/java/visitor/ASTPrinter.kt
+++ src/main/java/visitor/ASTPrinter.kt
@@ -14,74 +14,76 @@ import ast.statement.*
 class ASTPrinter: DefaultVisitor<String>() {
 
     override fun visit(program: Program): String {
-        TODO("Not yet implemented")
+        var result = "program ${program._identifier}\n"
+        program._procedures.forEach { result += it.accept(this) + "\n" }
+        result += "begin"
+        program._variables.forEach { result += it.accept(this) + "\n" }
+        program._statements.forEach { result += it.accept(this) + "\n"}
+        result += "end\n"
+
+        return result
     }
 
     override fun visit(procedure: Procedure): String {
-        TODO("Not yet implemented")
+        var result = "procedure ${procedure._name}("
+        procedure._variables.joinToString(", ") { it.accept(this) }
+        procedure._return?.let { result += ", res " + it.accept(this) }
+        result += ") begin\n"
+        procedure._statements.forEach { result += "\t" + it.accept(this) }
+        result += "end"
+
+        return result
     }
 
-    override fun visit(type: Type): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(type: Type) =
+        type.type._representation
 
-    override fun visit(position: Position): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(position: Position) =
+        "Position(${position._line}, ${position._column})"
 
-    override fun visit(variable: Variable): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(variable: Variable) =
+        "${variable._type.accept(this)} ${variable._name} ${variable._expression?.let { " = " + it.accept(this) }}"
 
-    override fun visit(variableBlock: VariableBlock): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(variableBlock: VariableBlock) =
+        if (variableBlock._variables.isEmpty()) {
+            ""
+        } else {
+            variableBlock._variables.first()._type.accept(this) + variableBlock._variables.joinToString(", ") { it._name }
+        }
 
-    override fun visit(unaryArithmeticExpression: UnaryArithmeticExpression): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(unaryArithmeticExpression: UnaryArithmeticExpression) =
+        "${unaryArithmeticExpression._operator._representation} ${unaryArithmeticExpression._expression.accept(this)}"
 
-    override fun visit(binaryArithmeticExpression: BinaryArithmeticExpression): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(binaryArithmeticExpression: BinaryArithmeticExpression) =
+        "${binaryArithmeticExpression._leftExpression.accept(this)} ${binaryArithmeticExpression._operator._representation} ${binaryArithmeticExpression._rightExpression.accept(this)}"
 
-    override fun visit(arithmeticConstant: ArithmeticConstant): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(arithmeticConstant: ArithmeticConstant) =
+        arithmeticConstant._value.toString()
 
-    override fun visit(arithmeticIdentifierExpression: IdentifierExpression): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(arithmeticIdentifierExpression: IdentifierExpression) =
+        arithmeticIdentifierExpression._identifier
 
-    override fun visit(unaryBooleanExpression: UnaryBooleanExpression): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(unaryBooleanExpression: UnaryBooleanExpression) =
+        "${unaryBooleanExpression._operator._representation} ${unaryBooleanExpression._expression.accept(this)}"
 
-    override fun visit(binaryBooleanExpression: BinaryBooleanExpression): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(binaryBooleanExpression: BinaryBooleanExpression) =
+        "${binaryBooleanExpression._leftExpression.accept(this)} ${binaryBooleanExpression._operator._representation} ${binaryBooleanExpression._rightExpression.accept(this)}"
 
-    override fun visit(booleanConstant: BooleanConstant): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(booleanConstant: BooleanConstant) =
+        booleanConstant._value.toString()
 
-    override fun visit(assignStatement: AssignStatement): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(assignStatement: AssignStatement) =
+        "${assignStatement._variableName} = ${assignStatement._value.accept(this)}"
 
-    override fun visit(callStatement: CallStatement) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(callStatement: CallStatement) =
+        "call ${callStatement._procedureName}${callStatement._arguments.joinToString(", ", prefix = "(", postfix = ")") { it.accept(this) }}"
 
-    override fun visit(ifStatement: IfStatement): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(ifStatement: IfStatement) =
+        "if ${ifStatement._condition.accept(this)} then\n${ifStatement._thenBody.accept(this)}\n${ifStatement._elseBody?.let { "else\n" + it.accept(this) }}"
 
-    override fun visit(skipStatement: SkipStatement): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(skipStatement: SkipStatement) =
+        "skip"
 
-    override fun visit(whileStatement: WhileStatement): String {
-        TODO("Not yet implemented")
-    }
+    override fun visit(whileStatement: WhileStatement) =
+        "while ${whileStatement._condition.accept(this)} do\n${whileStatement._body.accept(this)}"
 }
