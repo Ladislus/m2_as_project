0825280b04b6cdb06314f39a29a493308d46f053
Ladislus <walcak.ladislas@gmail.com>
2021-12-01 06:27:47 +0100

    [ADD] Added call to printer

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,9 +1,14 @@
 import utils.ExitCode
 import utils.buildAST
 import utils.exitWithCode
+import visitor.ASTPrinter
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) exitWithCode(ExitCode.NO_INPUT_FILE)
     val program = buildAST(args[0])
+    program?.let {
+        val printer = ASTPrinter()
+        println(printer.visit(it))
+    }
     exitWithCode(ExitCode.OK)
 }
