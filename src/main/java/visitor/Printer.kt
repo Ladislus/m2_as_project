4458a2c72fe22e27725842d1a48c07a931621d11
Ladislus <walcak.ladislas@gmail.com>
2021-12-02 14:05:01 +0100

    [ADD] Added minor fixes for Printer

--- src/main/java/visitor/Printer.kt
+++ src/main/java/visitor/Printer.kt
@@ -12,13 +12,12 @@ import ast.expression.bool.BooleanConstant
 import ast.expression.bool.UnaryBooleanExpression
 import ast.statement.*
 
-// TODO Implement (Question 2)
-class ASTPrinter: DefaultVisitor<String>() {
+class Printer: DefaultVisitor<String>() {
 
     override fun visit(program: Program): String {
         var result = "program ${program._identifier}\n"
         program._procedures.forEach { result += it.accept(this) + "\n" }
-        result += "begin"
+        result += "begin\n"
         program._variables.forEach { result += it.accept(this) + "\n" }
         program._statements.forEach { result += it.accept(this) + "\n"}
         result += "end\n"
@@ -28,10 +27,10 @@ class ASTPrinter: DefaultVisitor<String>() {
 
     override fun visit(procedure: Procedure): String {
         var result = "procedure ${procedure._name}("
-        procedure._variables.joinToString(", ") { it.accept(this) }
+        result += procedure._variables.joinToString(", ") { it.accept(this) }
         procedure._return?.let { result += ", res " + it.accept(this) }
-        result += ") begin\n"
-        procedure._statements.forEach { result += "\t" + it.accept(this) }
+        result += ")\nbegin\n"
+        procedure._statements.forEach { result += "\t" + it.accept(this) + "\n" }
         result += "end"
 
         return result
@@ -44,13 +43,17 @@ class ASTPrinter: DefaultVisitor<String>() {
         "Position(${position._line}, ${position._column})"
 
     override fun visit(variable: Variable) =
-        "${variable._type.accept(this)} ${variable._name} ${variable._expression?.let { " = " + it.accept(this) }}"
+        "${variable._type.accept(this)} ${variable._name}${variable._expression?.let { " := " + it.accept(this) } ?: ""}"
 
     override fun visit(variableBlock: VariableBlock) =
         if (variableBlock._variables.isEmpty()) {
             ""
         } else {
-            variableBlock._variables.first()._type.accept(this) + variableBlock._variables.joinToString(", ") { it._name }
+            "${variableBlock._variables.first()._type.accept(this)} ${variableBlock._variables.joinToString(", ") { variable ->
+                        variable._name + (variable._expression?.let { expression -> 
+                            " ${expression.accept(this)}" 
+                        } ?: "")
+                    }};"
         }
 
     override fun visit(unaryArithmeticExpression: UnaryArithmeticExpression) =
@@ -75,19 +78,19 @@ class ASTPrinter: DefaultVisitor<String>() {
         booleanConstant._value.toString()
 
     override fun visit(assignStatement: AssignStatement) =
-        "${assignStatement._variableName} = ${assignStatement._value.accept(this)}"
+        "${assignStatement._variableName} := ${assignStatement._value.accept(this)};"
 
     override fun visit(callStatement: CallStatement) =
         "call ${callStatement._procedureName}${callStatement._arguments.joinToString(", ", prefix = "(", postfix = ")") { it.accept(this) }}"
 
     override fun visit(ifStatement: IfStatement) =
-        "if ${ifStatement._condition.accept(this)} then\n${ifStatement._thenBody.accept(this)}\n${ifStatement._elseBody?.let { "else\n" + it.accept(this) }}"
+        "if (${ifStatement._condition.accept(this)}) then\n${ifStatement._thenBody.accept(this)}\n${ifStatement._elseBody?.let { "else\n" + it.accept(this) }}"
 
     override fun visit(skipStatement: SkipStatement) =
         "skip"
 
     override fun visit(whileStatement: WhileStatement) =
-        "while ${whileStatement._condition.accept(this)} do\n${whileStatement._body.accept(this)}"
+        "while (${whileStatement._condition.accept(this)}) do (\n${whileStatement._body.accept(this)}\n)"
 
     override fun visit(block: Block): String =
         block._statements.joinToString("\n") { "\t" + it.accept(this) }
