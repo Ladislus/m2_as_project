53e558021266f701f39d904fc337a18a0a77cd8c
Ladislus <walcak.ladislas@gmail.com>
2021-12-02 14:04:17 +0100

    [MOD] Renamed ASTPrinter to Printer for better comprehension

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,13 +1,13 @@
 import utils.ExitCode
 import utils.buildAST
 import utils.exitWithCode
-import visitor.ASTPrinter
+import visitor.Printer
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) exitWithCode(ExitCode.NO_INPUT_FILE)
     val program = buildAST(args[0])
     program?.let {
-        val printer = ASTPrinter()
+        val printer = Printer()
         println(printer.visit(it))
     }
     exitWithCode(ExitCode.OK)
