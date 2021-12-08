df3ab8121ad5a2638a5956cac1c4c43827c20ad0
Hervochon Corentin <corentin.hervochon@etu.univ-orleans.fr>
2021-12-08 16:35:52 +0100

    [MOD]completed DefaultVisitorAnalyse

--- src/main/java/visitor/analyse/DefaultVisitorAnalyse.kt
+++ src/main/java/visitor/analyse/DefaultVisitorAnalyse.kt
@@ -28,75 +28,23 @@ abstract class DefaultVisitorAnalyse(
         while (this._flow.hasNext()) this._flow.getNext().accept(this)
     }
 
-    override fun visit(procedure: Procedure) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(procedure: Procedure)  {}
 
-    override fun visit(type: Type) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(type: Type) {}
 
-    override fun visit(position: Position) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(position: Position) {}
 
-    override fun visit(block: Block) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(block: Block) {}
 
-    override fun visit(variable: Variable) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(variable: Variable) {}
 
-    override fun visit(variableBlock: VariableBlock) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(variableBlock: VariableBlock) {}
 
-    override fun visit(unaryArithmeticExpression: UnaryArithmeticExpression) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(callStatement: CallStatement) {}
 
-    override fun visit(binaryArithmeticExpression: BinaryArithmeticExpression) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(ifStatement: IfStatement) {}
 
-    override fun visit(arithmeticConstant: ArithmeticConstant) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(skipStatement: SkipStatement) {}
 
-    override fun visit(arithmeticIdentifierExpression: IdentifierExpression) {
-        TODO("Not yet implemented")
-    }
-
-    override fun visit(unaryBooleanExpression: UnaryBooleanExpression) {
-        TODO("Not yet implemented")
-    }
-
-    override fun visit(binaryBooleanExpression: BinaryBooleanExpression) {
-        TODO("Not yet implemented")
-    }
-
-    override fun visit(booleanConstant: BooleanConstant) {
-        TODO("Not yet implemented")
-    }
-
-    override fun visit(assignStatement: AssignStatement) {
-        TODO("Not yet implemented")
-    }
-
-    override fun visit(callStatement: CallStatement) {
-        TODO("Not yet implemented")
-    }
-
-    override fun visit(ifStatement: IfStatement) {
-        TODO("Not yet implemented")
-    }
-
-    override fun visit(skipStatement: SkipStatement) {
-        TODO("Not yet implemented")
-    }
-
-    override fun visit(whileStatement: WhileStatement) {
-        TODO("Not yet implemented")
-    }
+    override fun visit(whileStatement: WhileStatement) {}
 }
