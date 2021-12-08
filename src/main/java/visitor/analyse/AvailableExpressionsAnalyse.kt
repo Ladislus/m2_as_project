77abfb407c63a60cfeb98552acda89bc5c1a38be
Ribardiere Tom <tom.ribardiere@etu.univ-orleans.fr>
2021-12-08 17:42:05 +0100

    [WIP] Mise en place de Available Expression Analyse

--- src/main/java/visitor/analyse/AvailableExpressionsAnalyse.kt
+++ src/main/java/visitor/analyse/AvailableExpressionsAnalyse.kt
@@ -9,40 +9,58 @@ import ast.expression.bool.BinaryBooleanExpression
 import ast.expression.bool.BooleanConstant
 import ast.expression.bool.UnaryBooleanExpression
 import ast.statement.AssignStatement
+import java.beans.Expression
 
 class AvailableExpressionsAnalyse(
     _program: Program
 ) : DefaultVisitorAnalyse(_program) {
 
+    private val availableExpressions : Set<Expression>  = mutableSetOf()
+
     override fun visit(unaryArithmeticExpression: UnaryArithmeticExpression) {
-        TODO("Not yet implemented")
+        this.availableExpressions.plus(unaryArithmeticExpression)
     }
 
     override fun visit(binaryArithmeticExpression: BinaryArithmeticExpression) {
-        TODO("Not yet implemented")
+        this.availableExpressions.plus(binaryArithmeticExpression)
     }
 
     override fun visit(arithmeticConstant: ArithmeticConstant) {
-        TODO("Not yet implemented")
+        this.availableExpressions.plus(arithmeticConstant)
     }
 
     override fun visit(arithmeticIdentifierExpression: IdentifierExpression) {
-        TODO("Not yet implemented")
+        this.availableExpressions.plus(arithmeticIdentifierExpression)
     }
 
     override fun visit(unaryBooleanExpression: UnaryBooleanExpression) {
-        TODO("Not yet implemented")
+        this.availableExpressions.plus(unaryBooleanExpression)
     }
 
     override fun visit(binaryBooleanExpression: BinaryBooleanExpression) {
-        TODO("Not yet implemented")
+        this.availableExpressions.plus(binaryBooleanExpression)
     }
 
     override fun visit(booleanConstant: BooleanConstant) {
-        TODO("Not yet implemented")
+        this.availableExpressions.plus(booleanConstant)
     }
 
     override fun visit(assignStatement: AssignStatement) {
-        TODO("Not yet implemented")
+        val idenfier = assignStatement._variableName
+        for (expression in this.availableExpressions) {
+            kill(expression)
+        }
+    }
+    private fun kill (expression: Expression) {
+        val list = ArrayDeque(listOf(expression))
+        while (list.isNotEmpty()){
+            val current = list.removeFirst()
+            when(current){
+                is ArithmeticConstant -> {
+                    this.availableExpressions.remove(current)
+                }
+            }
+
+        }
     }
 }
