6110ab0edba517b1a8b128946a286a6120152652
Ladislus <walcak.ladislas@gmail.com>
2021-12-10 18:58:45 +0100

    [ADD] Added dot files to .gitignore

---
+++ src/main/java/visitor/flow/Flow.kt
@@ -0,0 +1,20 @@
+package visitor.flow
+
+import ast.*
+import ast.declaration.Variable
+import ast.declaration.VariableBlock
+import ast.expression.arithmetic.ArithmeticConstant
+import ast.expression.arithmetic.BinaryArithmeticExpression
+import ast.expression.arithmetic.IdentifierExpression
+import ast.expression.arithmetic.UnaryArithmeticExpression
+import ast.expression.bool.BinaryBooleanExpression
+import ast.expression.bool.BooleanConstant
+import ast.expression.bool.UnaryBooleanExpression
+import ast.statement.*
+
+class Flow(
+    _program: Program
+    ): DefaultFlow(_program) {
+
+
+}
