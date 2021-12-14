2832b5296f621f07857de5f12ef6615ccc8742b1
Ladislus <walcak.ladislas@gmail.com>
2021-12-14 15:44:35 +0100

    [MOD] Went from exits to entry

--- src/main/java/visitor/analyse/availableExpressions/AvailableExpressionsAnalyse.kt
+++ src/main/java/visitor/analyse/availableExpressions/AvailableExpressionsAnalyse.kt
@@ -28,43 +28,37 @@ class AvailableExpressionsAnalyse(
     private lateinit var _currentState: State
     private lateinit var _currentMemory: MutableSet<Expression>
 
-    private fun getPredecessorsMemory(predecessors: Set<State>): MutableSet<Expression> {
-        // Can't initialise candidate because an empty intersected with anything is the empty set
-        // (null represent bottom)
-        var candidate: MutableSet<Expression>? = null
-        for (predecessor in predecessors) {
-            candidate =
-                // If the candidate is Bottom...
-                if (candidate == null) {
-                    // ... we try to set it to the predecessor's memory (which can also be bottom)
-                    this._memory[predecessor]
-                // If it's not bottom ...
-                } else {
-                    // ... we try to intersect the two (if it's bottom, we keep the previous value of candidate)
-                    (this._memory[predecessor]?.intersect(candidate)?.toMutableSet()) ?: candidate
-                }
-        }
-        // Return the intersection of all predecessors (or an empty set if they all are bottom)
-        return candidate ?: mutableSetOf()
-    }
-
     override fun analyse() {
         // While we have state in the process stack
         while (this._flow.hasNext()) {
             // Get current state
             this._currentState = this._flow.getNext()
-            // Reset the value of the attribut (prevent everybody from having the same memory)
+            // Reset the value of the attribute (prevent everybody from having the same memory)
             this._currentMemory = mutableSetOf()
-            // Retrieve predecessors' memory
-            this._currentMemory += this.getPredecessorsMemory(this._currentState._predecessors)
-            // Launch analysis of the node
-            this._currentState._node.accept(this)
+
+            // Joint of the kill-gen of all the predecessors
+            var predecessorsKillGen: Set<Expression>? = null
+            this._currentState._predecessors.forEach { currentPredecessor ->
+                // If the predecessor is bottom, skip it
+                if (this._memory.containsKey(currentPredecessor)) {
+                    // Retrieve the memory of the predecessor
+                    this._currentMemory = this._memory[currentPredecessor]?.toMutableSet() ?: mutableSetOf()
+                    // KillGen of the predecessor
+                    currentPredecessor._node.accept(this)
+
+                    // Add it to the joint of all the predecessor's KillGen
+                    predecessorsKillGen = predecessorsKillGen?.intersect(this._currentMemory) ?: this._currentMemory
+
+                }
+            }
+            if (predecessorsKillGen == null) predecessorsKillGen = mutableSetOf()
+            this._currentMemory = predecessorsKillGen?.toMutableSet() ?: mutableSetOf()
 
             // If the previous memory is bottom (first time we analyse this state) ...
             if (!this._memory.containsKey(this._currentState)) {
                 // Update the state's memory
-                this._memory[this._currentState] = this._currentMemory
-                // Add all of it's sucessors to the process stack
+                this._memory[this._currentState] = this._currentMemory.toMutableSet()
+                // Add all of its successors to the process stack
                 this._flow.pileSuccessors(this._currentState)
             }
             // Else
@@ -72,12 +66,13 @@ class AvailableExpressionsAnalyse(
                 // Compare the previous memory with the new one
                 val previousMem = this._memory[this._currentState]!!
                 val newMem = this._currentMemory.intersect(previousMem)
+
                 // If they are different (the two contains are equivalents, but we're never too sure)
                 if ((newMem.size != previousMem.size) ||
                     !newMem.containsAll(previousMem) ||
                     !previousMem.containsAll(newMem)
                 ) {
-                    // Add all of it's sucessors to the process stack
+                    // Add all of its successors to the process stack
                     this._flow.pileSuccessors(this._currentState)
                     // Update its memory
                     this._memory[this._currentState] = newMem.toMutableSet()
@@ -86,7 +81,7 @@ class AvailableExpressionsAnalyse(
         }
 
         // Print the memory
-        println("Available expressions at exits:")
+        println("Available expressions at entry:")
         this._memory.forEach { (k: State, v: MutableSet<Expression>) ->
             println("\tState ${k._index} (\"${k._node.accept(this._printer)}\"): ${v.joinToString(separator = ", ", prefix = "[ ", postfix = " ]") { it.accept(this._printer) }}")
         }
@@ -177,6 +172,7 @@ class AvailableExpressionsAnalyse(
 
         // KILL
         val toDestroy = mutableSetOf<Expression>()
+
         // Retrieve all expressions to kill
         this._currentMemory.forEach { expression ->
             expression.accept(AvailableExpressionsKiller(assignStatement._variableName))?.forEach { foundExpression ->
