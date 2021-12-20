45f0a7c1cd8a1c802c2a35cf58d1d17744eeaafc
Ladislas WALCAK <walcak.ladislas@gmail.com>
2021-12-20 14:22:00 +0100

    [ADD] Added cache to the printer (prevent multiple visits of the same node)

--- src/main/java/visitor/printers/Printer.kt
+++ src/main/java/visitor/printers/Printer.kt
@@ -15,84 +15,95 @@ import visitor.DefaultVisitor
 
 class Printer: DefaultVisitor<String>() {
 
-    override fun visit(program: Program): String {
-        var result = "program ${program._identifier ?: ""}\n"
-        program._procedures.forEach { result += it.accept(this) + "\n" }
-        result += "begin\n"
-        program._variables.forEach { result += it.accept(this) + "\n" }
-        program._statements.forEach { result += it.accept(this) + "\n"}
-        result += "end\n"
+    private val _cache = mutableMapOf<Node, String>()
 
-        return result
+    override fun visit(program: Program): String {
+        return this._cache.getOrPut(program) {
+            var result = "program ${program._identifier ?: ""}\n"
+            program._procedures.forEach { result += it.accept(this) + "\n" }
+            result += "begin\n"
+            program._variables.forEach { result += it.accept(this) + "\n" }
+            program._statements.forEach { result += it.accept(this) + "\n"}
+            result += "end\n"
+
+            result
+        }
     }
 
     override fun visit(procedure: Procedure): String {
-        var result = "procedure ${procedure._name}("
-        result += procedure._variables.joinToString(", ") { it.accept(this) }
-        procedure._return?.let { result += ", res " + it.accept(this) }
-        result += ")\nbegin\n"
-        procedure._statements.forEach { result += "\t" + it.accept(this) + "\n" }
-        result += "end"
+        return this._cache.getOrPut(procedure) {
+            var result = "procedure ${procedure._name}("
+            result += procedure._variables.joinToString(", ") { it.accept(this) }
+            procedure._return?.let { result += ", res " + it.accept(this) }
+            result += ")\nbegin\n"
+            procedure._statements.forEach { result += "\t" + it.accept(this) + "\n" }
+            result += "end"
 
-        return result
+            result
+        }
     }
 
     override fun visit(type: Type) =
-        type.type._representation
+        this._cache.getOrPut(type) {
+            type.type._representation
+        }
 
     override fun visit(position: Position) =
         "Position(${position._line}, ${position._column})"
 
     override fun visit(variable: Variable) =
-        "${variable._type.accept(this)} ${variable._name}${variable._expression?.let { " := " + it.accept(this) } ?: ""}"
+        this._cache.getOrPut(variable) { "${variable._type.accept(this)} ${variable._name}${variable._expression?.let { " := " + it.accept(this) } ?: ""}" }
 
     override fun visit(variableBlock: VariableBlock) =
-        if (variableBlock._variables.isEmpty()) {
-            ""
-        } else {
-            "${variableBlock._variables.first()._type.accept(this)} ${variableBlock._variables.joinToString(", ") { variable ->
-                        variable._name + (variable._expression?.let { expression -> 
-                            " ${expression.accept(this)}" 
-                        } ?: "")
-                    }};"
+        this._cache.getOrPut(variableBlock) {
+            if (variableBlock._variables.isEmpty()) {
+                ""
+            } else {
+                "${variableBlock._variables.first()._type.accept(this)} ${variableBlock._variables.joinToString(", ") { variable ->
+                    variable._name + (variable._expression?.let { expression ->
+                        " ${expression.accept(this)}"
+                    } ?: "")
+                }};"
+            }
         }
 
     override fun visit(unaryArithmeticExpression: UnaryArithmeticExpression) =
-        "${unaryArithmeticExpression._operator._representation} ${unaryArithmeticExpression._expression.accept(this)}"
+        this._cache.getOrPut(unaryArithmeticExpression) { "${unaryArithmeticExpression._operator._representation} ${unaryArithmeticExpression._expression.accept(this)}" }
 
     override fun visit(binaryArithmeticExpression: BinaryArithmeticExpression) =
-        "${binaryArithmeticExpression._leftExpression.accept(this)} ${binaryArithmeticExpression._operator._representation} ${binaryArithmeticExpression._rightExpression.accept(this)}"
+        this._cache.getOrPut(binaryArithmeticExpression) { "${binaryArithmeticExpression._leftExpression.accept(this)} ${binaryArithmeticExpression._operator._representation} ${binaryArithmeticExpression._rightExpression.accept(this)}" }
 
     override fun visit(arithmeticConstant: ArithmeticConstant) =
-        arithmeticConstant._value.toString()
+        this._cache.getOrPut(arithmeticConstant) { arithmeticConstant._value.toString() }
 
     override fun visit(arithmeticIdentifierExpression: IdentifierExpression) =
-        arithmeticIdentifierExpression._identifier
+        this._cache.getOrPut(arithmeticIdentifierExpression) { arithmeticIdentifierExpression._identifier }
 
     override fun visit(unaryBooleanExpression: UnaryBooleanExpression) =
-        "${unaryBooleanExpression._operator._representation} ${unaryBooleanExpression._expression.accept(this)}"
+        this._cache.getOrPut(unaryBooleanExpression) { "${unaryBooleanExpression._operator._representation} ${unaryBooleanExpression._expression.accept(this)}" }
 
     override fun visit(binaryBooleanExpression: BinaryBooleanExpression) =
-        "${binaryBooleanExpression._leftExpression.accept(this)} ${binaryBooleanExpression._operator._representation} ${binaryBooleanExpression._rightExpression.accept(this)}"
+        this._cache.getOrPut(binaryBooleanExpression) { "${binaryBooleanExpression._leftExpression.accept(this)} ${binaryBooleanExpression._operator._representation} ${binaryBooleanExpression._rightExpression.accept(this)}" }
 
     override fun visit(booleanConstant: BooleanConstant) =
-        booleanConstant._value.toString()
+        this._cache.getOrPut(booleanConstant) { booleanConstant._value.toString() }
 
     override fun visit(assignStatement: AssignStatement) =
-        "${assignStatement._variableName} := ${assignStatement._value.accept(this)};"
+        this._cache.getOrPut(assignStatement) { "${assignStatement._variableName} := ${assignStatement._value.accept(this)};" }
 
     override fun visit(callStatement: CallStatement) =
-        "call ${callStatement._procedureName}${callStatement._arguments.joinToString(", ", prefix = "(", postfix = ")") { it.accept(this) }}"
+        this._cache.getOrPut(callStatement) { "call ${callStatement._procedureName}${callStatement._arguments.joinToString(", ", prefix = "(", postfix = ")") { it.accept(this) }}" }
 
     override fun visit(ifStatement: IfStatement) =
-        "if (${ifStatement._condition.accept(this)}) then\n${ifStatement._thenBody.accept(this)}\n${ifStatement._elseBody?.let { "else\n" + it.accept(this) }}"
+        this._cache.getOrPut(ifStatement) { "if (${ifStatement._condition.accept(this)}) then\n${ifStatement._thenBody.accept(this)}\n${ifStatement._elseBody?.let { "else\n" + it.accept(this) }}" }
 
     override fun visit(skipStatement: SkipStatement) =
-        "skip"
+        this._cache.getOrPut(skipStatement) { "skip" }
 
     override fun visit(whileStatement: WhileStatement) =
-        "while (${whileStatement._condition.accept(this)}) do (\n${whileStatement._body.accept(this)}\n)"
+        this._cache.getOrPut(whileStatement) { "while (${whileStatement._condition.accept(this)}) do (\n${whileStatement._body.accept(this)}\n)" }
 
     override fun visit(block: Block): String =
-        block._statements.joinToString("\n") { "\t" + it.accept(this) }
+        this._cache.getOrPut(block) { block._statements.joinToString("\n") { "\t" + it.accept(this) } }
 }
+
